6768f6442326069a04b8700bf0560e5644bdb9c5
millordisseny <74691679+millordisseny@users.noreply.github.com>
2021-06-15 13:10:15 -0500

    Add files via upload

---
+++ Reto1.py
@@ -0,0 +1,25 @@
+print("Bienvenido al sistema de ubicación para zonas públicas WIFI")
+
+username = int(input("Usuario:"))
+password = int(input("Contraseña:"))
+num_captcha1 = 739
+num_captcha2 = 7+(5*1)-9
+captcha = input(str(num_captcha1)+ "+" + str (num_captcha2)+ "= ")
+
+if username == int(51739):
+    password()
+else:
+    print("Error")
+
+if password == int(93715):
+    captcha
+else:
+    print ("Error")
+
+if captcha== int(742):
+   print("Sesión Iniciada")
+else:
+    print("Error")
+ 
+
+
